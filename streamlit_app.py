bda46b952077624ec126d875338a044345482e79
StellaRoche <112698024+StellaRoche@users.noreply.github.com>
2024-08-15 01:46:15 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -132,9 +132,7 @@ def file_upload():
         if uploaded_file:
             if uploaded_file.name.endswith('.csv'):
                 df = pd.read_csv(uploaded_file)
-            elif uploaded_file.name.endswith('.xlsx'):
-                df = pd.read_excel(uploaded_file, engine='openpyxl')
-
+            
             st.session_state.dataframes[uploaded_file.name] = df
 
             # Set file_uploaded to True
