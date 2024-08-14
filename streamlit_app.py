7f4f92cfdeded09c7ddbe0a651ce988d547f35d5
StellaRoche <112698024+StellaRoche@users.noreply.github.com>
2024-08-15 00:55:54 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -59,9 +59,9 @@ from sklearn.manifold import TSNE
 
 # Deep Learning Models
 #from keras.models import Sequential
-from tensorflow.keras.models import Model
-from tensorflow.keras.layers import Input, Dense
-from keras.utils import to_categorical
+#from tensorflow.keras.models import Model
+#from tensorflow.keras.layers import Input, Dense
+#from keras.utils import to_categorical
 # Utility Libraries
 import inspect
 import io
